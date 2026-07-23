58070787c98ab1e5f5a3fba87224d7fe002928a3
Rupesh <Rupeshkumar6410@gmail.com>
2026-07-23 11:05:51 +0530

    terraform statefile locking

---
+++ Day23/backend/main.tf
@@ -0,0 +1,25 @@
+resource "azurerm_resource_group" "backend" {
+  name     = "tfstate-rg"
+  location = "Central India"
+}
+
+resource "azurerm_storage_account" "backend" {
+
+  name                     = "rupeshtfstate12345"
+  resource_group_name      = azurerm_resource_group.backend.name
+  location                 = azurerm_resource_group.backend.location
+
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+}
+
+resource "azurerm_storage_container" "backend" {
+
+  name                  = "tfstate"
+
+  storage_account_id    = azurerm_storage_account.backend.id
+
+  container_access_type = "private"
+
+}
---
+++ Day23/infra/main.tf
@@ -0,0 +1,5 @@
+resource "azurerm_resource_group" "newresource_group" {
+  name     = "tfstate-rg-1234"
+  location = "Central India"
+  
+}
